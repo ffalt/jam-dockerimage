2295ae8f1541178c247ef4e6a9fa991cdb4e6aeb
ffalt <ffalty@gmail.com>
2020-05-09 13:27:17 +0200

    feat(docs): add more config descriptions

--- static/firststart.config.js
+++ static/firststart.config.js
@@ -7,15 +7,24 @@ module.exports = {
 	*/
 	adminUser: {
 		name: 'admin',
+		/*
+		   Since the default admin password is stored in clear in this file,
+		   you MUST change it on first login
+		*/
 		pass: 'admin'
 	},
 	/*
 		Default Media folders
+		Scan strategies:
+		'auto' -- try to figure it out
+		'artistalbum' -- artist/album folder structure
+		'compilation' -- bunch of compilation folders
+		'audiobook' -- bunch of audiobook folders
 	 */
 	roots: [
-		{name: 'Music', path: '/usr/share/media/music'},
-		{name: 'Compilations', path: '/usr/share/media/compilations'},
-		{name: 'Audiobooks', path: '/usr/share/media/audiobooks'},
-		{name: 'Soundtracks', path: '/usr/share/media/soundtrack'}
+		{name: 'Music', path: '/usr/share/media/music', strategy: 'auto'},
+		{name: 'Compilations', path: '/usr/share/media/compilations', strategy: 'compilation'},
+		{name: 'Audiobooks', path: '/usr/share/media/audiobooks', strategy: 'audiobook'},
+		{name: 'Soundtracks', path: '/usr/share/media/soundtrack', strategy: 'compilation'}
 	]
 };
