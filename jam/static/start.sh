be644c84eebe60c65f90a1829a9796c3078e24b2
ffalt <ffalty@gmail.com>
2020-08-30 17:46:51 +0200

    refactor(start): cleanup deprecated db waiting

--- jam/static/start.sh
+++ jam/static/start.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-
-if [ -n "$DATABASE_CHECK" ]
-then
-  while ! wget -q -O - "$DATABASE_CHECK"; do sleep 10; done;
-fi
-
 echo "Starting App"
-cd /home/node
+cd /home/node || exit
 pm2 start processes.json
 pm2 logs
