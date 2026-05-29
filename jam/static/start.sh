22d3584ff6df8e184bb859a2e7f8a8dfd745f12b
ffalt <ffalty@gmail.com>
2026-05-29 13:00:45 +0200

    build(jam): upgrade base image to node 26-bullseye-slim

--- jam/static/start.sh
+++ jam/static/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Starting App"
 cd /home/node/jam/serve/ || exit
-node --experimental-modules --es-module-specifier-resolution=node /home/node/jam/serve/dist/index.js
+node /home/node/jam/serve/dist/index.js
